cbaf0df31f290c71b221bf9da3cfa734bd611bd1
pragathish07 <pragarajesh779jd@gmail.com>
2026-03-11 13:46:46 +0530

    reverting

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -75,11 +75,19 @@ resource "aws_security_group" "sales_sg" {
   }
 
   ingress {
-    description = "HTTP from ALB only"
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
